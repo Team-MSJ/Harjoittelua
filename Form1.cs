1ed1fec17cae61c89cdcae063a29b020d57db676
Satu Suopanki <satu.suopanki@edu.sasky.fi>
2023-03-25 15:00:19 +0000

    Testaan Commitointia

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,14 @@ namespace Harjoittelua
 {
     public partial class Form1 : Form
     {
+        //  { }
+        // Tehdään Random-objekti to choose random ikonit neliöihin
+        // Webdings fontti on ikoneita, tehdään lista, jossa jokainen ikoni = kirjain esiintyy kahdesti
+
+        Random random = new Random();
+
+        List<string> icons = new List<string>()
+        {"M", "M", "S", "S", "T", "T", "i", "i", "a", "a", "t", "t", "7", "7", "H", "H"};
         public Form1()
         {
             InitializeComponent();
